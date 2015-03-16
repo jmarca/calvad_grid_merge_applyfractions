bb5bba87ab99872d91f008383fcd66054478b1db
James Marca <jmarca@translab.its.uci.edu>
2015-03-16 10:00:09 -0700

    export flatten.make_header_array from index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var routes = require('./lib/routes.js')
 var reduce = require('./lib/reduce.js')
+var flatten = require('./lib/flatten.js')
 
 exports.process_hpms_data = routes.process_hpms_data
 exports.reduce_handler = routes.reduce_handler
 exports.fractions_handler = routes.fractions_handler
 exports.grid_hpms_hourly_handler = routes.grid_hpms_hourly_handler
+exports.make_header_array=flatten.make_header_array
