1f5d4b37479a00f4646fa162f35e69b2e805858e
James E. Marca <james@activimetrics.com>
2016-04-12 17:49:08 -0700

    test that we aren't changing the input v,hh,nhh

--- test/test_reduce.js
+++ test/test_reduce.js
@@ -30,6 +30,28 @@ var test_db_unique = date.getHours()+'-'
                    + date.getMilliseconds()
 
 
+var header=["ts"
+           ,"freeway"
+           ,"n"
+           ,"hh"
+           ,"not_hh"
+           ,"o"
+           ,"avg_veh_spd"
+           ,"avg_hh_weight"
+           ,"avg_hh_axles"
+           ,"avg_hh_spd"
+           ,"avg_nh_weight"
+           ,"avg_nh_axles"
+           ,"avg_nh_spd"
+           ,"miles"
+           ,"lane_miles"
+           ,"detector_count"
+           ,"detectors"]
+var unmapper = {}
+for (var i=0,j=header.length; i<j; i++){
+    unmapper[header[i]]=i
+}
+
 before(function(done){
     config_okay(config_file,function(err,c){
         options.couchdb =_.clone(c.couchdb,true)
@@ -66,33 +88,43 @@ describe('reduce',function(){
         .await(function(e,d){
             should.not.exist(e)
             var memo = reduce.reduce({},task)
-            Object.keys(memo).should.have.lengthOf(744)
-            _.each(memo,function(v,ts){
-                v.should.have.keys(['detector_based'
-                                   ,'Urban Other Principal Arterial (OPA)'
-                                   ,'Urban Minor Arterial (MA)'
-                                   ,'Urban Collector (COL)'
-                                   ])
-                _.each(v,function(vv,road_type){
-                    if(road_type === 'detector_based'){
-                        vv.should.have.keys([ 'n',
-                                              'n_mt',
-                                              'hh_mt',
-                                              'nhh_mt',
-                                              'lane_miles'])
-                    }else{
+            var memokeys = Object.keys(memo)
+            memokeys.should.have.lengthOf(744)
+            fs.readFile('./test/files/189_72_2008_JAN.json',{'encoding':'utf8'},function(e,data){
+                var D = JSON.parse(data)
+                _.each(memokeys,function(ts,i){
+                    var v = memo[ts]
+                    v.should.have.keys(['detector_based'
+                                        ,'Urban Other Principal Arterial (OPA)'
+                                        ,'Urban Minor Arterial (MA)'
+                                        ,'Urban Collector (COL)'
+                                       ])
+                    _.each(v,function(vv,road_type){
+                        if(road_type === 'detector_based'){
+                            vv.should.have.keys([ 'n',
+                                                  'n_mt',
+                                                  'hh_mt',
+                                                  'nhh_mt',
+                                                  'lane_miles'])
+                            vv.n.should.eql(vv.n_mt)
+                            D.rows[i].doc.data[unmapper.n].should.eql(vv.n)
+                            D.rows[i].doc.data[unmapper.hh].should.eql(vv.hh_mt)
+                            D.rows[i].doc.data[unmapper.not_hh].should.eql(vv.nhh_mt)
+                        }else{
                         vv.should.have.keys([
                             'sum_lane_miles',
                             'sum_single_unit_mt',
                             'sum_vmt',
                             'sum_combination_mt'])
-                    }
-
+                        }
+                        return null
+                    })
+                    return null
                 })
-
-
+                done()
+                return null
             })
-            return done()
+            return null
         })
         return null;
     })
